e8fba6f23f72df6b8dff2f0f8be664be0131dfa7
herald-si <herald@sinetinformatica.it>
2022-01-10 15:43:51 +0100

    feature: scan results with debug mode
    scan with debug mode enabled returns error instead of NOT_EU_DCC
    scan with debug mode enabled returns disclaimer "DISABLE-DEBUG-MODE-IN-PRODUCTION" instead of person data

--- src/Utils/CertificateValidator.php
+++ src/Utils/CertificateValidator.php
@@ -11,19 +11,28 @@ class CertificateValidator
 {
 
     private $greenPassSimple;
-    
+
     private $scanMode;
 
     public function __construct(String $qrCodeText, String $scanMode = ValidationScanMode::CLASSIC_DGP)
     {
         $this->scanMode = $scanMode;
-        try{
+        try {
             $greenPass = Decoder::qrcode($qrCodeText);
-            $person = new SimplePerson($greenPass->holder->standardisedSurname, $greenPass->holder->surname, $greenPass->holder->standardisedForename, $greenPass->holder->forename);
-            
+
+            $debug = EnvConfig::isDebugEnabled();
+            $debugDisclaimer = "DISABLE-DEBUG-MODE-IN-PRODUCTION";
+
+            $standardizedFamilyName = $debug ? $debugDisclaimer : $greenPass->holder->standardisedSurname;
+            $familyName = $debug ? $debugDisclaimer : $greenPass->holder->surname;
+            $standardizedGivenName = $debug ? $debugDisclaimer : $greenPass->holder->standardisedForename;
+            $givenName = $debug ? $debugDisclaimer : $greenPass->holder->forename;
+
+            $person = new SimplePerson($standardizedFamilyName, $familyName, $standardizedGivenName, $givenName);
+
             $this->greenPassSimple = new CertificateSimple($person, $greenPass->holder->dateOfBirth, $greenPass->checkValid($this->scanMode));
-        }catch (\Exception $e){
-            $this->greenPassSimple = new CertificateSimple(null, null, ValidationStatus::NOT_EU_DCC);
+        } catch (\Exception $e) {
+            $this->greenPassSimple = new CertificateSimple(null, null, (EnvConfig::isDebugEnabled() ? $e : ValidationStatus::NOT_EU_DCC));
         }
     }
 
