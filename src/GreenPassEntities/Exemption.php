84bf91ae741badb6af6013435cd0f0ebbdce5fa2
herald-si <herald@sinetinformatica.it>
2022-01-12 16:48:03 +0100

    chore: formatted with php-cs-fixer

--- src/GreenPassEntities/Exemption.php
+++ src/GreenPassEntities/Exemption.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace Herald\GreenPass\GreenPassEntities;
 
 class Exemption extends CertificateType
 {
-
     /**
      * The first date on which the exemption is considered to be valid.
      */
@@ -17,14 +17,14 @@ class Exemption extends CertificateType
 
     public function __construct(array $data)
     {
-        $this->id = $data["e"][0]["ci"] ?? null;
+        $this->id = $data['e'][0]['ci'] ?? null;
 
-        $this->diseaseAgent = DiseaseAgent::resolveById($data["e"][0]["tg"]);
+        $this->diseaseAgent = DiseaseAgent::resolveById($data['e'][0]['tg']);
 
-        $this->country = $data["e"][0]["co"] ?? null;
-        $this->issuer = $data["e"][0]["is"] ?? null;
+        $this->country = $data['e'][0]['co'] ?? null;
+        $this->issuer = $data['e'][0]['is'] ?? null;
 
-        $this->validFrom = ! empty($data["e"][0]["df"]) ? new \DateTimeImmutable($data["e"][0]["df"]) : null;
-        $this->validUntil = ! empty($data["e"][0]["du"]) ? new \DateTimeImmutable($data["e"][0]["du"]) : null;
+        $this->validFrom = !empty($data['e'][0]['df']) ? new \DateTimeImmutable($data['e'][0]['df']) : null;
+        $this->validUntil = !empty($data['e'][0]['du']) ? new \DateTimeImmutable($data['e'][0]['du']) : null;
     }
 }
