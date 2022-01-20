dba32a68f44475a79e81f6ee94c62232785252f4
herald-si <herald@sinetinformatica.it>
2022-01-20 10:34:56 +0100

    feat: add new work scan mode

--- src/Validation/Covid19/ValidationRules.php
+++ src/Validation/Covid19/ValidationRules.php
@@ -1,33 +1,34 @@
 <?php
+
 namespace Herald\GreenPass\Validation\Covid19;
 
 class ValidationRules
 {
+    const RECOVERY_CERT_START_DAY = 'recovery_cert_start_day';
 
-    const RECOVERY_CERT_START_DAY = "recovery_cert_start_day";
-    
-    const RECOVERY_CERT_PV_START_DAY = "recovery_pv_cert_start_day";
+    const RECOVERY_CERT_PV_START_DAY = 'recovery_pv_cert_start_day';
 
-    const RECOVERY_CERT_END_DAY = "recovery_cert_end_day";
-    
-    const RECOVERY_CERT_PV_END_DAY = "recovery_pv_cert_end_day";
+    const RECOVERY_CERT_END_DAY = 'recovery_cert_end_day';
 
-    const MOLECULAR_TEST_START_HOUR = "molecular_test_start_hours";
+    const RECOVERY_CERT_PV_END_DAY = 'recovery_pv_cert_end_day';
 
-    const MOLECULAR_TEST_END_HOUR = "molecular_test_end_hours";
+    const MOLECULAR_TEST_START_HOUR = 'molecular_test_start_hours';
 
-    const RAPID_TEST_START_HOUR = "rapid_test_start_hours";
+    const MOLECULAR_TEST_END_HOUR = 'molecular_test_end_hours';
 
-    const RAPID_TEST_END_HOUR = "rapid_test_end_hours";
+    const RAPID_TEST_START_HOUR = 'rapid_test_start_hours';
 
-    const VACCINE_START_DAY_NOT_COMPLETE = "vaccine_start_day_not_complete";
+    const RAPID_TEST_END_HOUR = 'rapid_test_end_hours';
 
-    const VACCINE_END_DAY_NOT_COMPLETE = "vaccine_end_day_not_complete";
+    const VACCINE_START_DAY_NOT_COMPLETE = 'vaccine_start_day_not_complete';
 
-    const VACCINE_START_DAY_COMPLETE = "vaccine_start_day_complete";
+    const VACCINE_END_DAY_NOT_COMPLETE = 'vaccine_end_day_not_complete';
 
-    const VACCINE_END_DAY_COMPLETE = "vaccine_end_day_complete";
+    const VACCINE_START_DAY_COMPLETE = 'vaccine_start_day_complete';
 
-    const BLACK_LIST_UVCI = "black_list_uvci";
+    const VACCINE_END_DAY_COMPLETE = 'vaccine_end_day_complete';
 
+    const BLACK_LIST_UVCI = 'black_list_uvci';
+
+    const VACCINE_MANDATORY_AGE = 50;
 }
