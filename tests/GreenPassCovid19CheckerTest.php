3ded03ab9f58cc6d1247f798c31d947f74982ea6
herald-si <herald@sinetinformatica.it>
2022-01-20 10:37:18 +0100

    feat: add tests for new work scan mode

--- tests/GreenPassCovid19CheckerTest.php
+++ tests/GreenPassCovid19CheckerTest.php
@@ -1,33 +1,34 @@
 <?php
+
 namespace Herald\GreenPass\Validation\Covid19;
 
-use Herald\GreenPass\GreenPass;
 use Herald\GreenPass\GPDataTest;
-use Herald\GreenPass\Decoder\Decoder;
+use Herald\GreenPass\GreenPass;
 
 /**
  * GreenPassCovid19Checker test case.
  */
 class GreenPassCovid19CheckerTest extends \PHPUnit\Framework\TestCase
 {
+    const DATE_IN_5_MONTHS = '+5 month';
 
-    const DATE_IN_5_MONTHS = "+5 month";
+    const DATE_A_MONTH_AGO = '-1 month';
 
-    const DATE_A_MONTH_AGO = "-1 month";
+    const DATE_5_MONTHS_AGO = '-5 month';
 
-    const DATE_5_MONTHS_AGO = "-5 month";
+    const DATE_7_MONTHS_AGO = '-7 month';
 
-    const DATE_7_MONTHS_AGO = "-7 month";
+    const DATE_TOMORROW = '+1 day';
 
-    const DATE_TOMORROW = "+1 day";
+    const DATE_A_DAY_AGO = '-1 day';
 
-    const DATE_A_DAY_AGO = "-1 day";
+    const DATE_5_DAYS_AGO = '-5 day';
 
-    const DATE_5_DAYS_AGO = "-5 day";
+    const DATE_20_DAYS_AGO = '-20 day';
 
-    const DATE_20_DAYS_AGO = "-20 day";
+    const DATE_MORE_THAN_A_YEAR = '-366 day';
 
-    const DATE_MORE_THAN_A_YEAR = "-366 day";
+    const DATE_50_YEARS = '-50 year';
 
     protected $data_oggi;
 
@@ -41,11 +42,11 @@ class GreenPassCovid19CheckerTest extends \PHPUnit\Framework\TestCase
     {
         // TEST CODICE DIVERSO DA C19
         $testgp = GPDataTest::$vaccine;
-        $testgp["v"][0]["tg"] = "00000000000000";
+        $testgp['v'][0]['tg'] = '00000000000000';
         $greenpass = new GreenPass($testgp);
 
         $esito = GreenPassCovid19Checker::verifyCert($greenpass);
-        $this->assertEquals("NOT_COVID_19", $esito);
+        $this->assertEquals('NOT_COVID_19', $esito);
     }
 
     protected function tearDown(): void
@@ -54,4 +55,3 @@ class GreenPassCovid19CheckerTest extends \PHPUnit\Framework\TestCase
         parent::tearDown();
     }
 }
-
--- tests/TamponeCheckerTest.php
+++ tests/TamponeCheckerTest.php
@@ -1,66 +1,99 @@
 <?php
+
 namespace Herald\GreenPass\Validation\Covid19;
 
-use Herald\GreenPass\GreenPass;
 use Herald\GreenPass\GPDataTest;
-use Herald\GreenPass\Decoder\Decoder;
-use Herald\GreenPass\Validation\Covid19\GreenPassCovid19CheckerTest;
+use Herald\GreenPass\GreenPass;
 
 /**
  * TamponeCheckerTest test case.
  */
 class TamponeCheckerTest extends GreenPassCovid19CheckerTest
 {
-
     /**
-     * Test tampone dopo 12 ore
+     * Test tampone dopo 12 ore.
      */
     public function testVerifyCertTampone()
     {
         $testgp = GPDataTest::$testresult;
 
-        $data_greenpass = $this->data_oggi->modify("-12 hour");
-        $testgp["t"][0]["sc"] = $data_greenpass->format(\DateTime::ATOM);
+        $data_greenpass = $this->data_oggi->modify('-12 hour');
+        $testgp['t'][0]['sc'] = $data_greenpass->format(\DateTime::ATOM);
         $greenpass = new GreenPass($testgp);
 
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, "3G");
-        $this->assertEquals("VALID", $esito);
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '3G');
+        $this->assertEquals('VALID', $esito);
     }
 
     /**
-     * Test scan mode GreenPass
+     * Test scan mode GreenPass.
      */
     public function testScanModeTampone()
     {
         $testgp = GPDataTest::$testresult;
 
-        $data_greenpass = $this->data_oggi->modify("-12 hour");
-        $testgp["t"][0]["sc"] = $data_greenpass->format(\DateTime::ATOM);
+        $data_greenpass = $this->data_oggi->modify('-12 hour');
+        $testgp['t'][0]['sc'] = $data_greenpass->format(\DateTime::ATOM);
         $greenpass = new GreenPass($testgp);
 
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, "3G");
-        $this->assertEquals("VALID", $esito);
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '3G');
+        $this->assertEquals('VALID', $esito);
 
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, "2G");
-        $this->assertEquals("NOT_VALID", $esito);
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '2G');
+        $this->assertEquals('NOT_VALID', $esito);
 
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, "BOOSTED");
-        $this->assertEquals("NOT_VALID", $esito);
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'BOOSTED');
+        $this->assertEquals('NOT_VALID', $esito);
     }
 
     /**
-     * Test tampone dopo 120 ore
+     * Test work scan mode GreenPass.
+     */
+    public function testWorkScanModeTampone()
+    {
+        $testgp = GPDataTest::$testresult;
+
+        $data_greenpass = $this->data_oggi->modify('-12 hour');
+        $testgp['t'][0]['sc'] = $data_greenpass->format(\DateTime::ATOM);
+
+        $today_50_birthday = $this->data_oggi->modify(self::DATE_50_YEARS);
+
+        // birthday 50 years old
+        $testgp['dob'] = $today_50_birthday->format(\DateTime::ATOM);
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
+        $this->assertEquals('NOT_VALID', $esito);
+
+        // the day after 50 years old birthday
+        $today_50_birthday_plus_one = $today_50_birthday->modify('-1 day');
+        $testgp['dob'] = $today_50_birthday_plus_one->format(\DateTime::ATOM);
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
+        $this->assertEquals('NOT_VALID', $esito);
+
+        // the day before 50 years old birthday
+        $today_49_years_old = $today_50_birthday->modify('+1 day');
+        $testgp['dob'] = $today_49_years_old->format(\DateTime::ATOM);
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
+        $this->assertEquals('VALID', $esito);
+    }
+
+    /**
+     * Test tampone dopo 120 ore.
      */
     public function testTamponeScaduto()
     {
         $testgp = GPDataTest::$testresult;
 
-        $data_greenpass = $this->data_oggi->modify("-120 hour");
-        $testgp["t"][0]["sc"] = $data_greenpass->format(\DateTime::ATOM);
+        $data_greenpass = $this->data_oggi->modify('-120 hour');
+        $testgp['t'][0]['sc'] = $data_greenpass->format(\DateTime::ATOM);
         $greenpass = new GreenPass($testgp);
 
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, "3G");
-        $this->assertEquals("EXPIRED", $esito);
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '3G');
+        $this->assertEquals('EXPIRED', $esito);
     }
 }
-
