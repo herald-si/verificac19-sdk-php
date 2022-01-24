560e106eb1fef3b449b0c0b490a64ea12737cdd7
herald-si <herald@sinetinformatica.it>
2022-01-24 11:57:46 +0100

    feature: added exemption tests

---
+++ tests/GreenPassExemptionCheckerTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Herald\GreenPass\Validation\Covid19;
+
+use Herald\GreenPass\GPDataTest;
+use Herald\GreenPass\GreenPass;
+
+/**
+ * ExemptionCheckerTest test case.
+ */
+class GreenPassExemptionCheckerTest extends GreenPassCovid19CheckerTest
+{
+    /*
+     * Test exemption dopo un mese
+     */
+    public function testVerifyCertExemption()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_A_MONTH_AGO);
+        $data_scadenza_gp = $this->data_oggi->modify(self::DATE_IN_5_MONTHS);
+        $testgp['e'][0]['fr'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['du'] = $data_scadenza_gp->format('Y-m-d');
+
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass);
+        $this->assertEquals('VALID', $esito);
+    }
+
+    /*
+     * Test exemption super green pass
+     */
+    public function testSuperGreenPass()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_A_MONTH_AGO);
+        $data_scadenza_gp = $this->data_oggi->modify(self::DATE_IN_5_MONTHS);
+        $testgp['e'][0]['fr'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['du'] = $data_scadenza_gp->format('Y-m-d');
+
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '3G');
+        $this->assertEquals('VALID', $esito);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '2G');
+        $this->assertEquals('VALID', $esito);
+    }
+
+    /**
+     * Test Booster Scan mode.
+     */
+    public function testBoosterScanMode()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_A_MONTH_AGO);
+        $data_scadenza_gp = $this->data_oggi->modify(self::DATE_IN_5_MONTHS);
+        $testgp['e'][0]['fr'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['du'] = $data_scadenza_gp->format('Y-m-d');
+
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, '3G');
+        $this->assertEquals('VALID', $esito);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'BOOSTED');
+        $this->assertEquals('TEST_NEEDED', $esito);
+    }
+
+    /**
+     * Test exemption da domani.
+     */
+    public function testTomorrow()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_TOMORROW);
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass);
+        $this->assertEquals('NOT_VALID_YET', $esito);
+    }
+
+    /*
+     * test exemption dopo 5 mesi con date_until scaduto
+     */
+    public function testDateUntilScaduto()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_5_MONTHS_AGO);
+        $testgp['e'][0]['fr'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $data_fine_validita = $this->data_oggi->modify(self::DATE_A_DAY_AGO);
+        $testgp['e'][0]['du'] = $data_fine_validita->format('Y-m-d');
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass);
+        $this->assertEquals('NOT_VALID', $esito);
+    }
+
+    /*
+     * test exemption dopo 5 mesi con date_until valido
+     */
+    public function testDateUntilValid()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_5_MONTHS_AGO);
+        $testgp['e'][0]['fr'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $data_fine_validita = $this->data_oggi->modify(self::DATE_TOMORROW);
+        $testgp['e'][0]['du'] = $data_fine_validita->format('Y-m-d');
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass);
+        $this->assertEquals('VALID', $esito);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'BOOSTED');
+        $this->assertEquals('TEST_NEEDED', $esito);
+    }
+
+    /*
+     * test exemption dopo 7 mesi
+     */
+    public function testDueDate()
+    {
+        $testgp = GPDataTest::$exemption;
+        $data_greenpass = $this->data_oggi->modify(self::DATE_7_MONTHS_AGO);
+        $testgp['e'][0]['fr'] = $data_greenpass->format('Y-m-d');
+        $testgp['e'][0]['df'] = $data_greenpass->format('Y-m-d');
+        $greenpass = new GreenPass($testgp);
+
+        $esito = GreenPassCovid19Checker::verifyCert($greenpass);
+        $this->assertEquals('NOT_VALID', $esito);
+    }
+}
--- tests/data/GPDataTest.php
+++ tests/data/GPDataTest.php
@@ -1,14 +1,15 @@
 <?php
+
 namespace Herald\GreenPass;
 
 class GPDataTest
 {
     private const GP_CERTIFICATORE = 'Ministero della Salute';
-    
-    private const GP_VERSION = "1.0.0";
 
-    private const GP_TG = "840539006";
-    
+    private const GP_VERSION = '1.0.0';
+
+    private const GP_TG = '840539006';
+
     public static $qrcode_certificate_valid_but_revoked = 'HC1:6BFOXN%TSMAHN-H3YS1IK47ES6IXJR4E47X5*T917VF+UOGIS1RYZV:X9:IMJZTCV4*XUA2PSGH.+H$NI4L6HUC%UG/YL WO*Z7ON13:LHNG7H8H%BFP8FG4T 9OKGUXI$NIUZUK*RIMI4UUIMI.J9WVHWVH+ZEOV1AT1HRI2UHD4TR/S09T./08H0AT1EYHEQMIE9WT0K3M9UVZSVV*001HW%8UE9.955B9-NT0 2$$0X4PCY0+-CVYCRMTB*05*9O%0HJP7NVDEBO584DKH78$ZJ*DJWP42W5P0QMO6C8PL353X7H1RU0P48PCA7T5MCH5:ZJ::AKU2UM97H98$QP3R8BH9LV3*O-+DV8QJHHY4I4GWU-LU7T9.V+ T%UNUWUG+M.1KG%VWE94%ALU47$71MFZJU*HFW.6$X50*MSYOJT1MR96/1Z%FV3O-0RW/Q.GMCQS%NE';
 
     public static $qrcode_without_hc1 = '6BFOXN%TSMAHN-H3YS1IK47ES6IXJR4E47X5*T917VF+UOGIS1RYZV:X9:IMJZTCV4*XUA2PSGH.+H$NI4L6HUC%UG/YL WO*Z7ON13:LHNG7H8H%BFP8FG4T 9OKGUXI$NIUZUK*RIMI4UUIMI.J9WVHWVH+ZEOV1AT1HRI2UHD4TR/S09T./08H0AT1EYHEQMIE9WT0K3M9UVZSVV*001HW%8UE9.955B9-NT0 2$$0X4PCY0+-CVYCRMTB*05*9O%0HJP7NVDEBO584DKH78$ZJ*DJWP42W5P0QMO6C8PL353X7H1RU0P48PCA7T5MCH5:ZJ::AKU2UM97H98$QP3R8BH9LV3*O-+DV8QJHHY4I4GWU-LU7T9.V+ T%UNUWUG+M.1KG%VWE94%ALU47$71MFZJU*HFW.6$X50*MSYOJT1MR96/1Z%FV3O-0RW/Q.GMCQS%NE';
@@ -19,74 +20,96 @@ class GPDataTest
 
     public static $qrcode_ch = 'HC1:NCFK60DG0/3WUWGSLKH47GO0:S4KQDITFAUO9CK-500XK0JCV496F3JBS33S3F3MU394SY50.FK6ZK7:EDOLOPCO8F6%E3.DA%EOPC1G72A6YM86G77460A6TL6IL6G*8J*8:Q6E46VM8K:6 47FN8UPC0JCZ69FVCPD0LVC6JD846Y96E463W5.A6+EDG8F3I80/D6$CBECSUER:C2$NS346$C2%E9VC- CSUE145GB8JA5B$D% D3IA4W5646646-96:96.JCP9EJY8L/5M/5546.96SF63KC.SC4KCD3DX47B46IL6646H*6Z/ER2DD46JH8946JPCT3E5JDLA7$Q69464W51S6..DX%DZJC2/DYOA$$E5$C JC3/D9Z95LEZED1ECW.C8WE2OA3ZAGY8MPCG/DU2DRB8MTA8+9$PC5$CUZC$$5Y$5FBBC30.9V$*J7TUO*9T$MJ5FT9U:HMD$EUJD:IG/64QL452M4KJH8S7$9N:655W*:OY6M+M3GH0N4F9YN-W91UHUO8BBJH64H8N/5LN%05P0-KG87ONE58+M%N8FJJT9S+K65WRNN2D AM/H*UQ6SEBG71Q1R5H68KTL2R*FDP5DBGWEM18J45MS$5O.296HOMB9 CTTOZN8:MB95ITP045OUQ41AAB1K19W5L84%G%9H5Z57LRP$IH$K0PLLFCCNE8QG%8LRWN$1P$IA+3I:.3$JB:DUK%DQYIO5GRJF*$G*EMHV3RMLHISWW8FJIHGQ*:BV7N+CA:VVXOILXHDUJQL9LTNQ1THAB$EAGYU07V 89NNLA$NS7F8ENV:COAAJ+F-2NK+P-3';
 
-    public static $vaccine = Array(
-        "v" => Array(
-            "0" => Array(
-                "dn" => '2',
-                "ma" => 'ORG-100031184',
-                "vp" => '1119349007',
-                "dt" => '2021-08-13',
-                "co" => 'IT',
-                "ci" => 'FAKEID#0',
-                "mp" => 'EU/1/20/1507',
-                "is" => self::GP_CERTIFICATORE,
-                "sd" => '2',
-                "tg" => self::GP_TG
-            )
-        ),
-        "nam" => Array(
-            "fnt" => 'UTENTE',
-            "fn" => 'UTENTE',
-            "gnt" => 'TEST',
-            "gn" => 'TEST'
-        ),
-        "ver" => self::GP_VERSION,
-        "dob" => "1999-12-12"
-    );
+    public static $vaccine = [
+        'v' => [
+            '0' => [
+                'dn' => '2',
+                'ma' => 'ORG-100031184',
+                'vp' => '1119349007',
+                'dt' => '2021-08-13',
+                'co' => 'IT',
+                'ci' => 'FAKEID#0',
+                'mp' => 'EU/1/20/1507',
+                'is' => self::GP_CERTIFICATORE,
+                'sd' => '2',
+                'tg' => self::GP_TG,
+            ],
+        ],
+        'nam' => [
+            'fnt' => 'UTENTE',
+            'fn' => 'UTENTE',
+            'gnt' => 'TEST',
+            'gn' => 'TEST',
+        ],
+        'ver' => self::GP_VERSION,
+        'dob' => '1999-12-12',
+    ];
 
-    public static $testresult = Array(
-        "t" => Array(
-            "0" => Array(
-                "sc" => "2021-10-13T18:45:00+02:00",
-                "ma" => "1324",
-                "tt" => "LP217198-3",
-                "co" => "IT",
-                "tc" => "PROVA SNC",
-                "ci" => "TESTIDFAKE#2",
-                "is" => self::GP_CERTIFICATORE,
-                "tg" => self::GP_TG,
-                "tr" => "260415000"
-            )
-        ),
-        "nam" => Array(
-            "fnt" => 'UTENTE',
-            "fn" => 'UTENTE',
-            "gnt" => 'TEST',
-            "gn" => 'TEST'
-        ),
-        "ver" => self::GP_VERSION,
-        "dob" => "1989-11-15"
-    );
+    public static $testresult = [
+        't' => [
+            '0' => [
+                'sc' => '2021-10-13T18:45:00+02:00',
+                'ma' => '1324',
+                'tt' => 'LP217198-3',
+                'co' => 'IT',
+                'tc' => 'PROVA SNC',
+                'ci' => 'TESTIDFAKE#2',
+                'is' => self::GP_CERTIFICATORE,
+                'tg' => self::GP_TG,
+                'tr' => '260415000',
+            ],
+        ],
+        'nam' => [
+            'fnt' => 'UTENTE',
+            'fn' => 'UTENTE',
+            'gnt' => 'TEST',
+            'gn' => 'TEST',
+        ],
+        'ver' => self::GP_VERSION,
+        'dob' => '1989-11-15',
+    ];
 
-    public static $recovery = Array(
-        "r" => Array(
-            "0" => Array(
-                "fr" => "2021-01-13",
-                "df" => "2021-01-13",
-                "du" => "2021-12-13",
-                "co" => "IT",
-                "ci" => "TESTIDFAKERECOVERY#2",
-                "is" => self::GP_CERTIFICATORE,
-                "tg" => self::GP_TG
-            )
-        ),
-        "nam" => Array(
-            "fnt" => 'UTENTE',
-            "fn" => 'UTENTE',
-            "gnt" => 'TEST',
-            "gn" => 'TEST'
-        ),
-        "ver" => self::GP_VERSION,
-        "dob" => "1955-01-20"
-    );
+    public static $recovery = [
+        'r' => [
+            '0' => [
+                'fr' => '2021-01-13',
+                'df' => '2021-01-13',
+                'du' => '2021-12-13',
+                'co' => 'IT',
+                'ci' => 'TESTIDFAKERECOVERY#2',
+                'is' => self::GP_CERTIFICATORE,
+                'tg' => self::GP_TG,
+            ],
+        ],
+        'nam' => [
+            'fnt' => 'UTENTE',
+            'fn' => 'UTENTE',
+            'gnt' => 'TEST',
+            'gn' => 'TEST',
+        ],
+        'ver' => self::GP_VERSION,
+        'dob' => '1955-01-20',
+    ];
+
+    public static $exemption = [
+        'e' => [
+            '0' => [
+                'fr' => '2021-01-13',
+                'df' => '2021-01-13',
+                'du' => '2021-12-13',
+                'co' => 'IT',
+                'ci' => 'TESTIDFAKEEXEMPTION#2',
+                'is' => self::GP_CERTIFICATORE,
+                'tg' => self::GP_TG,
+            ],
+        ],
+        'nam' => [
+            'fnt' => 'UTENTE',
+            'fn' => 'UTENTE',
+            'gnt' => 'TEST',
+            'gn' => 'TEST',
+        ],
+        'ver' => self::GP_VERSION,
+        'dob' => '1955-01-20',
+    ];
 }
