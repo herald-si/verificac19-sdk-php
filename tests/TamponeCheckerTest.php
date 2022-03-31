c244bae565596345f6bd50e2faa8f514e910bb4b
herald-si <herald@sinetinformatica.it>
2022-03-31 11:38:14 +0200

    chore: removed tests for Tampone in WORK scanmode

--- tests/TamponeCheckerTest.php
+++ tests/TamponeCheckerTest.php
@@ -50,56 +50,6 @@ class TamponeCheckerTest extends GreenPassCovid19CheckerTest
     }
 
     /**
-     * Test work scan mode GreenPass.
-     */
-    public function testWorkScanModeTampone()
-    {
-        $testgp = GPDataTest::$testresult;
-
-        $data_greenpass = $this->data_oggi->modify(self::DATE_12_HOURS_AGO);
-        $testgp['t'][0]['sc'] = $data_greenpass->format(\DateTime::ATOM);
-
-        $today_50_birthday = $this->data_oggi->modify(self::DATE_50_YEARS);
-
-        // birthday 50 years old
-        $testgp['dob'] = $today_50_birthday->format(\DateTime::ATOM);
-        $greenpass = new GreenPass($testgp);
-
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
-        $this->assertEquals('VALID', $esito);
-
-        // the day after 50 years old birthday
-        $today_50_birthday_plus_one = $today_50_birthday->modify(self::DATE_A_DAY_AGO);
-        $testgp['dob'] = $today_50_birthday_plus_one->format(\DateTime::ATOM);
-        $greenpass = new GreenPass($testgp);
-
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
-        $this->assertEquals('NOT_VALID', $esito);
-
-        // the day before 50 years old birthday
-        $today_49_years_old = $today_50_birthday->modify(self::DATE_TOMORROW);
-        $testgp['dob'] = $today_49_years_old->format(\DateTime::ATOM);
-        $greenpass = new GreenPass($testgp);
-
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
-        $this->assertEquals('VALID', $esito);
-
-        // fixed young date
-        $testgp['dob'] = '2000-01-01';
-        $greenpass = new GreenPass($testgp);
-
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
-        $this->assertEquals('VALID', $esito);
-
-        // fixed old date
-        $testgp['dob'] = '1930-01-01';
-        $greenpass = new GreenPass($testgp);
-
-        $esito = GreenPassCovid19Checker::verifyCert($greenpass, 'WORK');
-        $this->assertEquals('NOT_VALID', $esito);
-    }
-
-    /**
      * Test tampone dopo 120 ore.
      */
     public function testTamponeScaduto()
